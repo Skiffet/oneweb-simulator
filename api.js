6e2fa4b5637fc5d691587a5e9d30264c32c0ebb2
Thanutham <113492833+Skiffet@users.noreply.github.com>
2026-07-17 09:38:22 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,3 @@
-// ==========================================
-// TODO: เปลี่ยนเป็น IP ของ GPU server
-// หา IP ได้โดยพิมพ์ ip a บนเครื่อง server
-// ==========================================
 export const API_URL = "http://192.168.100.219:3000";
 
 export async function apiPost(path, body) {
